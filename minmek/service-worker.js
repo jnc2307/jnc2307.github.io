f1f7592e386be1e126275eda6d585e30c020d7a8
jnc2307 <contact.innovativeapps@gmail.com>
2020-04-24 16:44:18 +0530

    jnc

--- minmek/service-worker.js
+++ minmek/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3b6d0680d9de95001ee8977309dd318a.js"
+  "./precache-manifest.30cdcc1ec21db50be4ed9e4adb7d5860.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
